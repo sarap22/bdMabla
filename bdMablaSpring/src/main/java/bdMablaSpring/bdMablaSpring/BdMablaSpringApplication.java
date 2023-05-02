bf2904798d2247b7707b40f62e528cb4bddecde6
Sara <saritaprieto445@gmail.com>
2023-05-02 16:25:09 -0500

    Jpa de Mabla

---
+++ bdMablaSpring/src/main/java/bdMablaSpring/bdMablaSpring/BdMablaSpringApplication.java
@@ -0,0 +1,13 @@
+package bdMablaSpring.bdMablaSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdMablaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdMablaSpringApplication.class, args);
+	}
+
+}
---
+++ bdMablaSpring/src/test/java/bdMablaSpring/bdMablaSpring/BdMablaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package bdMablaSpring.bdMablaSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdMablaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
